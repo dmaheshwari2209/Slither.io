411c38118318eaa723015ff26e96b6a791c53578
dmaheshwari2209 <100116359+dmaheshwari2209@users.noreply.github.com>
2023-05-31 20:02:40 +0530

    client network transform and basic player movement with mouse

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerController : NetworkBehaviour
+{
+    [SerializeField] private float speed = 3f;
+
+    private Camera _mainCamera;
+    private Vector3 _mouseInput;
+
+    private void Update() {
+        if(!Application.isFocused) return;
+        
+        // Movement
+        _mouseInput.x = Input.mousePosition.x;
+        _mouseInput.y = Input.mousePosition.y;
+        _mouseInput.z = _mainCamera.nearClipPlane;
+
+        Vector3 mouseWorldCoordinates = _mainCamera.ScreenToWorldPoint(_mouseInput);
+        transform.position = Vector3.MoveTowards(current:transform.position, target:mouseWorldCoordinates, maxDistanceDelta:Time.deltaTime * speed);
+
+        // Rotation
+        if(mouseWorldCoordinates != transform.position){
+            Vector3 targetDirection = mouseWorldCoordinates - transform.position;
+            transform.up = targetDirection;
+        }
+    }
+
+    public override void OnNetworkSpawn() {
+        base.OnNetworkSpawn();
+        Initialize();
+    }
+
+    private void Initialize() {
+        _mainCamera = Camera.main;
+    }
+}
