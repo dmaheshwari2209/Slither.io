1a87ecbe7440900cc19ba123dfe9833d54f547bd
dmaheshwari2209 <100116359+dmaheshwari2209@users.noreply.github.com>
2023-06-02 16:33:17 +0530

    tail prefab and logic

---
+++ Assets/_Scripts/PlayerLength.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerLength : NetworkBehaviour
+{
+    [SerializeField] private GameObject tailPrefab;
+
+    public NetworkVariable<ushort> length = new(1);
+    private Collider2D _collider2D;
+    private Transform _lastTail;
+
+    private List<GameObject> _tails;
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        _tails = new List<GameObject>();
+        _lastTail = transform;
+        _collider2D = GetComponent<Collider2D>();
+        Debug.Log("Last tail set");
+        if (!IsServer) length.OnValueChanged += LengthChanged;
+    }
+
+    [ContextMenu("Add Length")]
+    private void AddLength()
+    {
+        length.Value += 1;
+        InstantiateTail();
+    }
+
+    private void LengthChanged(ushort previousValue, ushort newValue)
+    {
+        InstantiateTail();
+    }
+
+    private void InstantiateTail()
+    {
+        var tailGameObject = Instantiate(tailPrefab, transform.position, Quaternion.identity);
+        tailGameObject.GetComponent<SpriteRenderer>().sortingOrder = -length.Value;
+        if (tailGameObject.TryGetComponent(out Tail tail))
+        {
+            tail.networkedOwner = transform;
+            tail.followTransform = _lastTail;
+            _lastTail = tailGameObject.transform;
+            Physics2D.IgnoreCollision(tailGameObject.GetComponent<Collider2D>(), _collider2D);
+        }
+
+        _tails.Add(tailGameObject);
+    }
+}
---
+++ Assets/_Scripts/Tail.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tail : MonoBehaviour
+{
+    [SerializeField] private float delayTime = 0.1f;
+    [SerializeField] private float distance = 0.3f;
+    [SerializeField] private float moveStep = 10f;
+
+    public Transform followTransform;
+    public Transform networkedOwner;
+
+    private Vector3 _targetPosition;
+
+    private void Update() {
+        _targetPosition = followTransform.position - followTransform.forward * distance;
+        _targetPosition += (transform.position - _targetPosition) * delayTime;
+        _targetPosition.z = 0f;
+        transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * moveStep);
+    }
+    
+}
